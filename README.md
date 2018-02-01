0ca0495b3d105e202537777e00b67be35703b1ba
Martyn Janes <martyn@obany.com>
2018-02-01 09:08:14 +0300

    Added getNodeInfo Tutorial

---
+++ README.md
@@ -0,0 +1,16 @@
+# IOTA Pico Framework Tutorials
+
+A set of tutorials to help use the IOTA Pico Framework libraries.
+
+## Tutorials
+
+* [Simple API getNodeInfo](./getNodeInfo/README.md)
+
+## Examples
+
+For more complex examples see the following repos.
+
+* Browser Examples written in JavaScript [@iota-pico/examples-browser-js](https://github.com/iotaeco/iota-pico-examples-browser-js)
+* Browser Examples written in TypeScript [@iota-pico/examples-browser-ts](https://github.com/iotaeco/iota-pico-examples-browser-ts)
+* NodeJS CLI Examples written in JavaScript [@iota-pico/examples-nodejs-js](https://github.com/iotaeco/iota-pico-examples-nodejs-js)
+* NodeJS CLI Examples written in TypeScript [@iota-pico/examples-nodejs-ts](https://github.com/iotaeco/iota-pico-examples-nodejs-ts)
---
+++ getNodeInfo/getNodeInfo.js
@@ -0,0 +1,26 @@
+// Import the classes we need
+const core = require("@iota-pico/core");
+const api = require("@iota-pico/api");
+const pal = require("@iota-pico/pal-nodejs");
+
+(async function () {
+    // Create an end point to communicate with the node
+    const networkEndpoint = new core.NetworkEndPoint("http", "n1.iota.eco", undefined, 14265);
+
+    // Create a network client from the PAL
+    const networkClient = new pal.NetworkClient(networkEndpoint);
+
+    // Create an API client using the network client and the API version
+    const apiClient = new api.ApiClient(networkClient, "1");
+
+    try {
+        // Make the call to the API
+        const response = await apiClient.getNodeInfo();
+
+        // And log the response
+        console.log(response);
+    } catch(err) {
+        // Or log an error if it failed
+        console.log(err);
+    }
+})();
